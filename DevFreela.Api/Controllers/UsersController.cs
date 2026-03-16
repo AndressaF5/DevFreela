01ba7ee2731694a641836710dc6a945e925165eb
andressaf5 <andressa@DARK-LADY>
2026-03-16 17:06:45 -0300

    Iniciando projeto, adicionando controllers, actions e classes models

---
+++ DevFreela.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DevFreela.Api/Models/CreateProjectInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevFreela.Api.Models
+{
+    public class CreateProjectInputModel
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int IdClient { get; set; }
+        public int IdFreelancer { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
