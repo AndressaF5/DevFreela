a252e905d1711508192b5824d34224a9bf990251
andressaf5 <andressa@DARK-LADY>
2026-04-08 17:40:11 -0300

    Criando projeto de testes e adicionando métodos para validações

---
+++ TestProjectDevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace TestProjectDevFreela.UnitTests.Core
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ProjectIsCreated_Start_Success()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descrição do Projeto", 1, 2, 10000);
+
+            // Act
+            project.Start();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+            Assert.True(project.Status == ProjectStatusEnum.InProgress);
+            Assert.False(project.StartedAt is null);
+        }
+
+        [Fact]
+        public void ProjectIsInInvalidState_Start_ThrowsException()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descrição do Projeto", 1, 2, 10000);
+            project.Start();
+
+            // Act + Assert
+            Action? start = project.Start;
+
+            var exception = Assert.Throws<InvalidOperationException>(start);
+            Assert.Equal(Project.INVALID_STATE_MESSAGE, exception.Message);
+        }
+    }
+}
