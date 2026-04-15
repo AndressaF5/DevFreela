c9fd6f9f3b9ac217c4f4a213f562351c7d9b9d99
andressaf5 <andressa@DARK-LADY>
2026-04-14 22:03:13 -0300

    Adicionando método para salvar senha de forma segura

--- DevFreela.Infrastructure/Auth/AuthService.cs
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DevFreela.Infrastructure.Auth
@@ -17,7 +18,21 @@ namespace DevFreela.Infrastructure.Auth
 
         public string ComputeHash(string password)
         {
-            throw new NotImplementedException();
+            using (var hash = SHA256.Create())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                var hashBytes = hash.ComputeHash(passwordBytes);
+
+                var builder = new StringBuilder();
+
+                for (var i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
         }
 
         public string GenerateToken(string email, string role)
